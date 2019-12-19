c210556d4c632ba703efb4c9ab48fb15d7501555
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2019-12-19 12:59:48 +0100

    get old tweets filtering

--- scripts/harvest/app.js
+++ scripts/harvest/app.js
@@ -4,22 +4,54 @@ const Twitter = require('twitter');
 const config = require('./config.js');
 const path = require('path');
 const fs = require('fs');
+const csv = require('csv-parser');
 
-// Read the GetOldTweets data
-//joining path of directory 
-const directoryPath = path.join(__dirname, '../data/got');
-//passing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file); 
-    });
-});
+got_to_id_list()
+
+function got_to_id_list() {
+	let options = {}
+	options.limit = Infinity // For testing purpose
+
+	const outputDirPath = path.join(__dirname, '../data');
+	let idListStream = fs.createWriteStream(outputDirPath +'/got_id_list.txt');
+	let rejectedStream = fs.createWriteStream(outputDirPath +'/got_rejected_log.txt');
+
+	// Read the GetOldTweets data
+	// joining path of directory 
+	const directoryPath = path.join(__dirname, '../data/got');
+	// passing directoryPath and callback function
+	fs.readdir(directoryPath, function (err, files) {
+	  //handling error
+	  if (err) {
+	  	return console.log('Unable to scan directory: ' + err);
+	  } 
+		let filesCount = files.length
+	  //listing all files using forEach
+	  files.forEach(function (file, fi) {
+	  	fs.createReadStream(directoryPath +'/'+ file)
+			  .pipe(csv({separator:';'}))
+			  .on('data', (row) => {
+			  	if (options.limit-->0) {
+				    // console.log(row)
+				  	if (gotTextContentOrdeal(row.text)) {
+					  	idListStream.write(row.id+'\n')
+					  	// console.log('YES '+row.text)
+					  } else {
+					  	rejectedStream.write(row.text+'\n')
+					  	// console.log('NO  '+row.text)
+					  }
+			  	}
+			  })
+			  .on('end', () => {
+			  	if (filesCount-- == 0) {
+						idListStream.end()
+						rejectedStream.end()
+			  	}
+			    console.log('CSV file successfully processed');
+			  })
+	  })
+	})
+}
 
 const T = new Twitter(config);
 
@@ -39,3 +71,16 @@ T.get('statuses/lookup', params, function(err, data, response) {
   }
 })
 */
+
+function gotTextContentOrdeal(text){
+	if (text == undefined) return false
+	if (text.substr(0,3) == "RT ") return false
+	if (text.match(/ok.?.?boomer/gi)) {
+		// Remove url STUB
+		text = text.replace(/https?:\/\/.*/i, '')
+		// Remove image
+		text = text.replace(/pic\.twitter\.com\/[^ ]*/i, '')
+		if (text.length > 20) return false
+		else return true
+	} else return false
+}
