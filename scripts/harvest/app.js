3064d7e32e7a0f004a9f1b8b15a341de76d93bb7
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2019-12-19 11:53:47 +0100

    read files

--- scripts/harvest/app.js
+++ scripts/harvest/app.js
@@ -1,9 +1,29 @@
 // See https://codeburst.io/build-a-simple-twitter-bot-with-node-js-in-just-38-lines-of-code-ed92db9eb078
 
-var Twitter = require('twitter');
-var config = require('./config.js');
-var T = new Twitter(config);
+const Twitter = require('twitter');
+const config = require('./config.js');
+const path = require('path');
+const fs = require('fs');
 
+// Read the GetOldTweets data
+//joining path of directory 
+const directoryPath = path.join(__dirname, '../data/got');
+//passing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
+
+const T = new Twitter(config);
+
+/*
 var id_list = [20]
 
 // Set up your search parameters
@@ -18,3 +38,4 @@ T.get('statuses/lookup', params, function(err, data, response) {
     console.log(err);
   }
 })
+*/
