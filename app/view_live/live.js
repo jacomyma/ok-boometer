8e274207dc7527edac96df8c421fd2e24608afe6
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2019-12-21 22:40:09 +0100

    live avocado in your face

--- app/view_live/live.js
+++ app/view_live/live.js
@@ -17,7 +17,7 @@ angular.module('okboometer.view_live', ['ngRoute'])
 	// Set up initial loop
 	var liveLoop
 	$timeout(function(){
-		liveLoop = $interval(updateLive, 10000)
+		liveLoop = $interval(updateLive, 5000)
 		$scope.$on('$destroy', function() {
 	    // Make sure that the interval is destroyed
 	    if (angular.isDefined(liveLoop)) {
@@ -37,10 +37,10 @@ angular.module('okboometer.view_live', ['ngRoute'])
 
 				data = data
 					// Remove those too old (>90 sec)
-					/*.filter(function(row){
+					.filter(function(row){
 						var then = new Date(row['Date'])
 						return now-then<(90000) // 90 seconds
-					})*/
+					})
 					// Compare with memory to remove the already used ones
 					.filter(function(row){
 						return !cache.seenBoomings[row['Booming tweet ID']]
@@ -54,7 +54,6 @@ angular.module('okboometer.view_live', ['ngRoute'])
 				// Sort by time
 				data.sort(function(a, b){ return b.time - a.time  })
 
-
 				if (data.length>0) {
 					$timeout(function(){
 						// Take the most recent
@@ -62,23 +61,82 @@ angular.module('okboometer.view_live', ['ngRoute'])
 
 						// remember it
 						cache.seenBoomings[$scope.booming['Booming tweet ID']] = true
+
+						// Wait and throw an avocado
+						resetAvocado()
+						$timeout(throwAvocado, 600)
+
 					})
 				}
-
-				// Load it
-
-				// Wait
-
-				// Throw an avocado
-
-				// Load the booming tweet over it
-
-
 			})
+  }
 
+	var avocado = document.getElementById('avocado')
+  var avocadoLoop
+  var throwing = {r:0, speed:50, g:0.0015}
+  function throwAvocado() {
+  	// Place avocado by reverse-launching
+		var tweetDiv = document.getElementsByClassName('tweet-container')[0]
+		var rect = tweetDiv.getBoundingClientRect()
+		throwing.x = rect.left + Math.random() * (rect.right - rect.left)
+		throwing.y = rect.top + Math.random() * (rect.bottom - rect.top)
 
+		// Set up throwing
+		throwing.dr = ((Math.random()<0.5)?(+1):(-1))*(0.1+0.4*Math.random())
+		throwing.dx = ((Math.random()<0.5)?(+1):(-1))*(0.7+0.3*Math.random())
+		throwing.dy = -Math.abs(Math.sin(Math.acos(throwing.dx)))
+  	throwing.step = 0
+		// Simulate throwing
+		while ((throwing.x>=-200 && throwing.x<=(window.innerWidth||1600)+200)) {
+			throwing.dy += throwing.speed * throwing.g
+	  	throwing.x += throwing.speed * throwing.dx
+	  	throwing.y += throwing.speed * throwing.dy
+	  	throwing.step++
+		}
+		throwing.dx = -throwing.dx
+		throwing.dy = -throwing.dy
 
+  	// Display
+  	avocado.style.display = 'block'
+  	avocado.style.left = Math.round(throwing.x - avocado.offsetWidth/2)+'px'
+  	avocado.style.top = Math.round(throwing.y - avocado.offsetHeight/2)+'px'
 
+  	avocadoLoop = $interval(updateAvocado, 33)
+		$scope.$on('$destroy', function() {
+	    // Make sure that the interval is destroyed
+	    if (angular.isDefined(avocadoLoop)) {
+	      $interval.cancel(avocadoLoop)
+	      avocadoLoop = undefined
+	    }
+	  })
+  }
+
+  function resetAvocado() {
+  	avocado.style.display = 'none'
+  	avocado.src = 'img/avocado-color.svg'
+  }
+
+  function updateAvocado() {
+  	throwing.dy += throwing.speed * throwing.g
+  	throwing.x += throwing.speed * throwing.dx
+  	throwing.y += throwing.speed * throwing.dy
+  	throwing.r += throwing.speed * throwing.dr
+  	throwing.step--
+  	// rotate
+  	avocado.style.webkitTransform = 'rotate('+throwing.r+'deg)'; 
+    avocado.style.mozTransform    = 'rotate('+throwing.r+'deg)'; 
+    avocado.style.msTransform     = 'rotate('+throwing.r+'deg)'; 
+    avocado.style.oTransform      = 'rotate('+throwing.r+'deg)'; 
+    avocado.style.transform       = 'rotate('+throwing.r+'deg)'; 
+    // Place
+  	avocado.style.left = Math.round(throwing.x - avocado.offsetWidth/2)+'px'
+  	avocado.style.top = Math.round(throwing.y - avocado.offsetHeight/2)+'px'
+  	// End?
+  	if (throwing.step <= 0) {
+	  	avocado.src = 'img/crashed-avocado-color.svg'
+  		$interval.cancel(avocadoLoop)
+	    avocadoLoop = undefined
+  	}
   }
 	
 })
