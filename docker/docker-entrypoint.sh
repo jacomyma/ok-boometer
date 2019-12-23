4262e1be8fad104e9e898b5a4bfff2690011454c
Benoit Simard <contact@bsimard.com>
2019-12-23 10:29:40 +0100

    Correct sed regex for the data ingestion

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,10 +8,10 @@ npm run build
 
 cd /ok-boometer/scripts/JS
 cp config.example.js config.js
-sed -i "s#@FILLME_CONSUMER_KEY@#${TWITTER_CONSUMER_KEY}#g" /ok-boometer/scripts/JS/config.js
-sed -i "s#@FILLME_CONSUMER_SECRET@#${TWITTER_CONSUMER_SECRET}#g" /ok-boometer/scripts/JS/config.js
-sed -i "s#@FILLME_ACCESS_TOKEN_KEY@#${TWITTER_ACCESS_TOKEN_KEY}#g" /ok-boometer/scripts/JS/config.js
-sed -i "s#@FILLME_ACCES_TOKEN_SECRET@#${TWITTER_ACCES_TOKEN_SECRET}#g" /ok-boometer/scripts/JS/config.js
+sed -i "s#FILLME_CONSUMER_KEY#${TWITTER_CONSUMER_KEY}#g" /ok-boometer/scripts/JS/config.js
+sed -i "s#FILLME_CONSUMER_SECRET#${TWITTER_CONSUMER_SECRET}#g" /ok-boometer/scripts/JS/config.js
+sed -i "s#FILLME_ACCESS_TOKEN_KEY#${TWITTER_ACCESS_TOKEN_KEY}#g" /ok-boometer/scripts/JS/config.js
+sed -i "s#FILLME_ACCES_TOKEN_SECRET#${TWITTER_ACCES_TOKEN_SECRET}#g" /ok-boometer/scripts/JS/config.js
 npm install
 npm install -g forever
 forever start stream.js
