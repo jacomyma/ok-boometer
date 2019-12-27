6424954f4b1267324b004b4db28e6834cec7a4d1
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2019-12-28 00:12:21 +0100

    boomed view

--- app/view_boomed/boomed.js
+++ app/view_boomed/boomed.js
@@ -15,7 +15,41 @@ angular.module('okboometer.view_boomed', ['ngRoute'])
 	$scope.loaded = false
 	$scope.boomedId = decodeURIComponent($routeParams.boomed)
 
-	dataProvider.onLoad(function(data){
+	$scope.load = function(){
+		$scope.loaded = false
+		dataProvider.load('boomedUsers', true, function(data){
+			$scope.boomedScoreByUser = {}
+			data.forEach(function(d){
+				$scope.boomedScoreByUser[d['id']] = d['score']
+			})
+			dataProvider.load('usernameIndex', false, function(data){
+				$scope.usernameIndex = {}
+				data.forEach(function(d){
+					$scope.usernameIndex[d['User ID']] = d['User name']
+				})
+				dataProvider.load('whoTweetedIndex', false, function(data){
+					$scope.whoTweetedIndex = {}
+					data.forEach(function(d){
+						$scope.whoTweetedIndex[d['Tweet ID']] = d['User ID']
+					})
+					dataProvider.load('boomedTweets', true, function(data){
+						$scope.boomedTweets = data.filter(function(d){
+							return $scope.whoTweetedIndex[d.id] == $scope.boomedId
+						})
+						$scope.loaded = true
+					})
+				})
+			})
+		})
+	}
+
+	$scope.$watch(function(){
+    return cache.timeMode;
+  }, function(){
+		$scope.load()
+	})
+
+/*	dataProvider.onLoad(function(data){
 		$timeout(function(){
 			$scope.data = data
 			$scope.loaded = true
@@ -34,5 +68,5 @@ angular.module('okboometer.view_boomed', ['ngRoute'])
 			dataProvider.load(cache.timeMode)
 		})
 	})
-	
+*/	
 })
